55c76f06db46f909e3f49c30e6e3c6212ed91651
dmontefalcon <dmontefalcon@ucsd.edu>
2022-07-07 08:50:46 -0700

    testfile 1 works

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,26 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.ArrayList;
+
+
 import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+    public void test1() throws IOException {
+        String testFile = Files.readString(Path.of("/Users/darri/OneDrive/Documents/GitHub/markdown-parser-1/test-file.md"));
+        ArrayList<String> testThis = MarkdownParse.getLinks(testFile);
+        assertEquals(testThis, List.of("https://something.com", "some-thing.html"));
     }
 
+    @Test
+    public void test2() throws IOException {
+        String testFile = Files.readString(Path.of("/Users/darri/OneDrive/Documents/GitHub/markdown-parser-1/test-file2.md"));
+        ArrayList<String> testThis = MarkdownParse.getLinks(testFile);
+        assertEquals(testThis, List.of("https://google.com", "some-thing.html"));
+    }
 }
